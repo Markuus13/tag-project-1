e6871289536685fabb0007416643223528e88316
Leonardo Almeida <leonardodealm@gmail.com>
2017-08-26 10:25:40 -0300

    Adding edges without Segmentation Fault.

--- Graph.cpp
+++ Graph.cpp
@@ -45,39 +45,39 @@ public:
         head = newNode;
     }
 
-    // vertexNode * findVertex(int v) {
-    //     vertexNode * currentNode = graphHead -> next;
-    //
-    //     if(currentNode == NULL) return NULL;
-    //
-    //     while(currentNode -> vertex != v) {
-    //         if(currentNode -> next == NULL){
-    //             return NULL;
-    //         } else {
-    //             currentNode = currentNode -> next;
-    //         }
-    //     }
-    //     return currentNode;
-    // }
-    //
-    // listNode * newListNode(int endV) {
-    //     listNode * newNode = new listNode;
-    //     newNode -> endVertex = endV;
-    //     newNode -> next = NULL;
-    //     return newNode;
-    // }
-    //
-    // void addNewEdge(int startV, int endV) {
-    //     listNode * newNode = newListNode(endV);
-    //     vertexNode * originVertex = findVertex(startV);
-    //     newNode -> next = originVertex -> listHead;
-    //     originVertex -> listHead = newNode;
-    //
-    //     newNode = newListNode(startV);
-    //     originVertex = findVertex(endV);
-    //     newNode -> next = originVertex -> listHead;
-    //     originVertex -> listHead = newNode;
-    // }
+    vertexNode * findVertex(int v) {
+        vertexNode * current = head;
+
+        if(current == NULL) return NULL;
+
+        while(current -> vertex != v) {
+            if(current -> next == NULL) {
+                return NULL;
+            } else {
+                current = current -> next;
+            }
+        }
+        return current;
+    }
+
+    listNode * newListNode(int vertex) {
+        listNode * newNode = new listNode;
+        newNode -> dest = vertex;
+        newNode -> next = NULL;
+        return newNode;
+    }
+
+    void addNewEdge(int startV, int endV) {
+        listNode * newNode = newListNode(endV);
+        vertexNode * originVertex = findVertex(startV);
+        newNode -> next = originVertex -> listHead;
+        originVertex -> listHead = newNode;
+
+        newNode = newListNode(startV);
+        originVertex = findVertex(endV);
+        newNode -> next = originVertex -> listHead;
+        originVertex -> listHead = newNode;        
+    }
 };
 
 int main(int argc, char const *argv[]) {
@@ -85,6 +85,6 @@ int main(int argc, char const *argv[]) {
     Graph G;
     G.addNewVertex(1);
     G.addNewVertex(2);
-    G.printGraph();
+    G.addNewEdge(1, 2);
     return 0;
 }
