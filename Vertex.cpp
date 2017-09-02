92b0020d51b1f87df8eebf72ab3ff009278e1a3d
Leonardo Almeida <leonardodealm@gmail.com>
2017-09-02 11:58:30 -0300

    First attempt to refactoring.

--- Vertex.cpp
+++ Vertex.cpp
@@ -1,100 +1,85 @@
-// g++ -std=c++11 -Wall -pedantic Vertex.cpp -o vertex-main
-
-#include <iostream>
-#include <vector>
-#include <fstream>
-#include <sstream>
-#include <algorithm>
-
+/* g++ -std=c++11 -Wall -pedantic Vertex.cpp -o vertex-main */
+#include <bits/stdc++.h>
 using namespace std;
 
 class Vertex {
-  private:
+private:
     string registration;
     string name;
-    vector<Vertex> adjacencyVertices;
+    vector<Vertex> adjVertices;
 
-  public:
-    Vertex() {}
-
-    Vertex(string registration, string name) {
-      this->registration = registration;
-      this->name = name;
-    }
-
-    string getRegistration() {
-      return this->registration;
-    }
-
-    void setRegistration(int registration) {
-      this->registration = registration;
-    }
-
-    string getName() {
-      return this->name;
-    }
-
-    void setName(string name) {
-      this->name = name;
-    }
-
-    vector<Vertex> getAdjacencyVertices() {
-      return this->adjacencyVertices;
-    }
-
-    void addAdjacencyVertex(Vertex vertex) {
-    //   cout << vertex.getName() << endl;
-      this->adjacencyVertices.push_back(vertex);
-    }
+ public:
+     Vertex() {}
+     Vertex(string registration, string name) {
+         this->registration = registration;
+         this->name = name;
+     }
+     string getRegistration() {
+         return this->registration;
+     }
+     void setRegistration(int registration) {
+         this->registration = registration;
+     }
+     string getName() {
+         return this->name;
+     }
+     void setName(string name) {
+         this->name = name;
+     }
+     vector<Vertex> getAdjVertices() {
+         return this->adjVertices;
+     }
+     void addAdjacencyVertex(Vertex vertex) {
+         this->adjVertices.push_back(vertex);
+     }
 };
 
 class Graph {
-  private:
+private:
     vector<Vertex> vertices;
-  public:
+public:
     vector<Vertex> getVertices() {
-      return this->vertices;
+        return this->vertices;
     }
-
     void addVertex(Vertex vertex) {
-      this->vertices.push_back(vertex);
+        this->vertices.push_back(vertex);
+    }
+    Vertex findByRegistration(string registration) {
+        for (Vertex vertex: this->vertices) {
+            if(vertex.getRegistration() == registration) {
+                return vertex;
+            }
+        }
+        return Vertex("9999999999", "LEOZIN DA MASSA");
     }
 
-    Vertex find_by_registration(string registration) {
-      for (Vertex vertex: this->vertices) {
-        if(vertex.getRegistration() == registration) {
-          return vertex;
+    void formatedPrint() {
+        cout << "endereco bugado " << &this->vertices << endl;
+        for (Vertex vertex: this->vertices) {
+          cout << "Nome: " << vertex.getName() << "|";
+          cout << "Matrícula: " << vertex.getRegistration() << "|";
+          cout << "Amigos: ";
+          for (Vertex friend_vertex :vertex.getAdjVertices()) {
+            cout << friend_vertex.getName() << " ";
+          }
+          cout << endl;
         }
-      }
-      return Vertex("9999999999", "LEOZIN DA MASSA");
-    }
-
-    void formated_print() {
-      for (Vertex vertex: this->vertices) {
-        cout << "Nome: " << vertex.getName() << "|";
-        cout << "Matrícula: " << vertex.getRegistration() << "|";
-        cout << "Amigos: ";
-        for (Vertex friend_vertex :vertex.getAdjacencyVertices()) {
-          cout << friend_vertex.getName() << " ";
-        }
-        cout << endl;
-      }
     }
 };
 
 vector<string> split(string str, char delimiter) {
-  vector<string> internal;
-  stringstream ss(str); // Turn the string into a stream.
-  string token;
+    vector<string> internal;
+    stringstream ss(str); // Turn the string into a stream.
+    string token;
 
-  while(getline(ss, token, delimiter)) {
-    internal.push_back(token);
-  }
-
-  return internal;;
+    while(getline(ss, token, delimiter)) {
+        internal.push_back(token);
+    }
+    return internal;;
 }
 
-Graph create_graph_from_file() {
+
+Graph createGraphFromFile() {
     Graph graph;
     ifstream myfile;
     myfile.open("teste.txt");
@@ -116,13 +101,25 @@ Graph create_graph_from_file() {
         }
 
         int i = 0;
-        for (vector<string> row_friend: friends_cache) {
-            for (string friend_registration: row_friend) {
-                Vertex friend_vertex = graph.find_by_registration(friend_registration);
-                graph.getVertices()[i].addAdjacencyVertex(friend_vertex);
-            }
-            i++;
+        vector<Vertex> all = graph.getVertices();
+        cout << "Endereco ok:" << &all << endl;
+        for(Vertex v: all) {
+          vector<string> current = friends_cache[i];
+                for (string friend_registration: current) {
+                    Vertex friend_vertex = graph.findByRegistration(friend_registration);
+                    v.addAdjacencyVertex(friend_vertex);
+                }
+                cout << "Nome: " << v.getName() << "|";
+                cout << "Matrícula: " << v.getRegistration() << "|";
+                cout << "Amigos: ";
+                i++;
+                vector<Vertex> oi =  v.getAdjVertices();
+                for(Vertex k: oi) {
+                  cout << k.getRegistration() << " ";
+                }
+                cout << endl;
         }
+        cout << &graph;
         myfile.close();
     } else {
         cout << "Unable to open file.";
@@ -131,6 +128,8 @@ Graph create_graph_from_file() {
 }
 
 int main() {
-    Graph studentsGraph = create_graph_from_file();
-    studentsGraph.formated_print();
+    Graph studentsGraph = createGraphFromFile();
+    studentsGraph.formatedPrint();
+
+    return 0;
 }
