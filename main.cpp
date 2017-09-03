420ff46e16ce8974587dcb87394013c60af15984
Marcus Vinícius <marcus.f22@gmail.com>
2017-09-02 22:18:15 -0300

    Renamed data file.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
-#include <bits/stdc++.h>
 #include "Vertex.h"
 #include "Graph.h"
 
 using namespace std;
 
+
 int main() {
     Graph studentsGraph;
 
-    studentsGraph.createVertices("amigos.txt");
-    studentsGraph.createAdjVertices("amigos.txt");
+    studentsGraph.createVertices("amigos_tag20172.txt");
+    studentsGraph.createAdjVertices("amigos_tag20172.txt");
     studentsGraph.formatedPrint();
 
     return 0;
