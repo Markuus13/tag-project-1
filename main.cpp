984d0f6832cdd3031f426d0c599bb847dceb8a06
Marcus Vinícius <marcus.f22@gmail.com>
2017-09-04 16:55:55 -0300

    BronKerbosch initialized.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include "Vertex.h"
 #include "Graph.h"
 
+using namespace std;
+
+vector<vector<Vertex>> maximalCliques;
+
+vector<Vertex> verticesIntersec(vector<Vertex> first, vector<Vertex> second) {
+    vector<Vertex> intersec;
+    for (Vertex a: first) {
+        for (Vertex b: second) {
+            if (a.getRegistration() == b.getRegistration())
+                intersec.push_back(a);
+        }
+    }
+    return intersec;
+}
+
+void bronKerbosch(vector<Vertex> R, vector<Vertex> P, vector<Vertex> X) {
+    if (P.empty() && X.empty()) {
+        maximalCliques.push_back(R);
+        return;
+    }
+
+    for (Vertex v: P) {
+        R.push_back(v);
+        bronKerbosch(R, verticesIntersec(P, v.getAdjVertices()), verticesIntersec(X, v.getAdjVertices()));
+        // P.erase(remove(P.begin(), P.end(), v), P.end());
+        X.push_back(v);
+    }
+}
+
 int main() {
     Graph studentsGraph;
 
@@ -8,6 +37,14 @@ int main() {
     studentsGraph.createAdjVertices("doc/amigos_tag20172.txt");
     studentsGraph.sortDesc();
     studentsGraph.printRegistrations();
+
+    cout << "----------------------------------" << endl;
+
+    // vector<Vertex> x = studentsGraph.getVertices();
+    // vector<Vertex> v;
+    // vector<Vertex> z;
+    //
+    // bronKerbosch(&v, &x, &z);
 
     return 0;
 }
